5560742f734ac9cc2ad84933551b6789c63287dd
Dmitriy <larkingit@yandex.ru>
2019-10-02 17:55:12 +0700

    переворот строки

--- src/main/java/Lesson_3/WebinarCodeSamples/Main.java
+++ src/main/java/Lesson_3/WebinarCodeSamples/Main.java
@@ -1,5 +1,12 @@
 package Lesson_3.WebinarCodeSamples;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,5 +16,19 @@ public class Main {
         System.out.println(myStack.toString());
         System.out.println(myStack.pop(10));
         System.out.println(myStack.toString());
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            StringBuffer s = new StringBuffer(reader.readLine());
+            System.out.println(s.reverse());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
