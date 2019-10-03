6dc04a88e9dfcfe1688eb0e498791d2e1c119024
Dmitriy <larkingit@yandex.ru>
2019-10-03 09:47:21 +0700

    -лишние импорты

--- src/main/java/Lesson_3/WebinarCodeSamples/Main.java
+++ src/main/java/Lesson_3/WebinarCodeSamples/Main.java
@@ -3,9 +3,7 @@ package Lesson_3.WebinarCodeSamples;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
+
 
 public class Main {
 
