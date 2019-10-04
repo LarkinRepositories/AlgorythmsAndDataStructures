0c15094a7ba72f69a1c5a575857e99163aff24b8
Dmitriy <larkingit@yandex.ru>
2019-10-04 15:55:10 +0700

    реализована дека. TODO: исправить ситуацию с приведением типов.

---
+++ src/main/java/Lesson_3/HomeWork/MyDeque.java
@@ -0,0 +1,38 @@
+package Lesson_3.HomeWork;
+
+import java.util.ArrayList;
+
+public class MyDeque<T> extends MyQueue {
+    private final int DEFAULT_CAPACITY = 10;
+    private int capacity;
+    private ArrayList<T> list;
+
+    public MyDeque() {
+        list = super.getList();
+    }
+
+    public MyDeque(int capacity) {
+        super(capacity);
+        this.list = super.getList();
+    }
+
+    public T peekLast() {
+        if (this.list != null) return this.list.get(list.size()-1);
+        else throw new NullPointerException();
+    }
+
+    public void insertFront(T item) {
+        if (this.list !=null) this.list.add(0, item);
+        else throw new NullPointerException();
+    }
+
+    public void removeFront() {
+        if (this.list !=null) this.list.remove(this.list.get(0));
+    }
+
+    @Override
+    public String toString() {
+        return this.list.toString();
+    }
+
+}
