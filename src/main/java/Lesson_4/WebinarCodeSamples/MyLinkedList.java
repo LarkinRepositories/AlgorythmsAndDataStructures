191a07660305f3f9ba495d27533864d376cff049
Dmitriy <larkingit@yandex.ru>
2019-10-07 17:29:03 +0700

    вообще не понял почему не работает - разобраться

--- src/main/java/Lesson_4/WebinarCodeSamples/MyLinkedList.java
+++ src/main/java/Lesson_4/WebinarCodeSamples/MyLinkedList.java
@@ -1,92 +1,68 @@
 package Lesson_4.WebinarCodeSamples;
 
-import com.sun.istack.internal.NotNull;
-import lombok.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-import java.util.Collection;
-
-//использован lombok, краткий FAQ: https://habr.com/ru/post/438870/
-
-@Getter @EqualsAndHashCode @ToString
+@Getter @ToString
 public class MyLinkedList<T> {
     @Setter
-    private Node firstElement = null;
+    private Node first = null;
+    @Setter
+    private Node last = null;
     private int size = 0;
 
 
-    public boolean isEmpty() {
-        return this.firstElement == null;
-    }
-
-    public T getFirstElementValue() {
-        return this.firstElement.getValue();
-    }
-
     public void add(T item) {
         Node newNode = new Node(item);
-//        newNode.setNextElement(this.getFirstElement());
-//        this.setFirstElement(newNode);
-        if (isEmpty()) this.firstElement = newNode;
-        firstElement.setNextElement(newNode);
+        if (isEmpty()) addFirst(newNode);
+        first.setNext(newNode);
         size++;
     }
 
     public void add(int index, T item) {
-        if (index < 0 || index > size) add(item);
-        if (index == 0) addFirst(item);
-        else {
-            Node current = firstElement;
-            for (int i = 0; i < index - 1; i++) {
-                current = current.getNextElement();
-            }
-            Node newNode = new Node(item);
-            newNode.setNextElement(current.getNextElement());
-            current.setNextElement(newNode);
-            size++;
-        }
-    }
-
-
-
-    public void remove() throws Exception {
-        if (isEmpty()) throw new Exception("The list is empty");
-        Node oldFirst = firstElement;
-        firstElement = firstElement.getNextElement();
-        size--;
-    }
-
-    public int indexOf(T item) {
-        Node current = firstElement;
-        int index = 0;
-        while (current != null) {
-            if (current.getValue().equals(item)) return index;
-            current = current.getNextElement();
-            index++;
-        }
-        return -1;
-    }
-
-    public boolean contains(T item) {
-        return indexOf(item) != -1;
-    }
-
-
-    private void addFirst(T item) {
         Node newNode = new Node(item);
-        newNode.setNextElement(this.getFirstElement());
-        this.setFirstElement(newNode);
+        if (index < 0 || index > size) add(item);
+        if (index == 0) addFirst(newNode);
+
+    }
+
+    public T removeFirst() {
+        if (isEmpty()) return  null;
+        Node oldFirst = first;
+        first = first.next;
+        size--;
+        return oldFirst.value;
+    }
+
+    private void addFirst(Node node) {
+        node.setNext(first);
+        first = node;
         size++;
     }
 
+
+    private boolean isEmpty() {
+        return (first == null) && (last == null);
+    }
+
+
     @Data
     private class Node {
-
         private T value;
-        private Node nextElement;
+        private Node next = null;
+        private Node previous = null;
 
-        Node(T value) {
-            this.value = value;
+        Node(T item) {
+            this.value = item;
         }
 
+//        @Override
+//        public String toString() {
+//            return "Node{" +
+//                    "value=" + value +
+//                    '}';
+//        }
     }
 }
