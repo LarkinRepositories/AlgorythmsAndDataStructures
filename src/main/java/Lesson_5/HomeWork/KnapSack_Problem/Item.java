8e4f3f63623939d22476df7f6d714c6bd4a0e301
nineserpents <43865893+nineserpents@users.noreply.github.com>
2019-10-11 19:12:51 +0700

    теперь реализует Comparable для сортировки

--- src/main/java/Lesson_5/HomeWork/KnapSack_Problem/Item.java
+++ src/main/java/Lesson_5/HomeWork/KnapSack_Problem/Item.java
@@ -3,12 +3,19 @@ package Lesson_5.HomeWork.KnapSack_Problem;
 import lombok.Data;
 
 @Data
-public class Item {
-    private int price;
-    private int weight;
+public class Item implements Comparable<Item> {
+    private Integer price;
+    private Integer weight;
 
-    public Item(int price, int weight) {
+    public Item(Integer price, Integer weight) {
         this.price = price;
         this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Item item) {
+        Integer currentDelta = this.price / this.weight;
+        Integer itemDelta = item.price / item.weight;
+        return currentDelta.compareTo(itemDelta);
     }
 }
