f8ff6d89e676453937a095d7a3b2954f63b23940
Dmitriy <larkingit@yandex.ru>
2019-10-21 15:34:08 +0700

    сэмплы кода с вебинара

---
+++ src/main/java/Lesson_8/WebinarCodeSamples/Main.java
@@ -0,0 +1,14 @@
+package Lesson_8.WebinarCodeSamples;
+
+public class Main {
+    public static void main(String[] args) {
+        ChainingHashMap<Integer, String> map = new ChainingHashMap<>();
+        map.put(1, "one");
+        map.put(2, "two");
+        map.put(3, "three");
+        map.put(12, "twelwe");
+        map.put(15, "fiftheen");
+
+        System.out.println(map);
+    }
+}
